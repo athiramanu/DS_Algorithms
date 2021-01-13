74e941d49261485c57de05bebc5bd08a756e7646
Athira Manu <athiramanu9400@gmail.com>
2021-01-13 07:37:51 +0530

    binary tree inorder traversal with recursion

---
+++ stack/binary-tree-inorder-traversal/binary-tree-inorder-traversal-recursion.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root, result = []) {
+    if (typeof root != 'undefined' && root != null) {
+        root.left && inorderTraversal(root.left, result);
+        result.push(root.val);
+        root.right && inorderTraversal(root.right, result);
+    }    
+    return result;
+};
