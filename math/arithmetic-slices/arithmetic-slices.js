24a037324b8d3ba14d7bae94c78586adc4c4370d
athiramanu <athiramanu9400@gmail.com>
2021-02-19 08:57:03 +0530

    # Arithmetic Slices

---
+++ math/arithmetic-slices/arithmetic-slices.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var numberOfArithmeticSlices = function(A) {
+    var result = 0;
+    var diff, i, j;
+    
+    if (A.length < 3) {
+        return result;
+    }
+    
+    for (i = 1, j = 0; i < A.length; i++) {
+        if (isNaN(diff)) {
+            diff = A[i] - A[i-1];
+        }
+        else if ((A[i] - A[i-1]) != diff) {
+            result = (i-j) >= 3 ? result + getCombinationCount(i-j) : result;
+            j = i-1;
+            diff = A[i] - A[i-1];   
+        }
+    }
+    
+    result += getCombinationCount(i-j);
+    
+    return result;
+};
+
+
+var getCombinationCount = function(count) {
+    var comb = 0;
+    var i = 1;
+    
+    while (count  >= 3) {
+        comb += i;
+        count--;
+        i++;
+    }
+    return comb;
+}
