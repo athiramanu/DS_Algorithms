1968ca18f921e1dbaeb0e44d8f2b5cb57ec95813
athiramanu <athiramanu9400@gmail.com>
2021-02-22 15:15:41 +0530

    Broken Calculator

---
+++ recursion_and_backtracking/broken-calculator/broken-calculator.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} X
+ * @param {number} Y
+ * @return {number}
+ */
+var brokenCalc = function(X, Y, steps = 0) {    
+    if (X >= Y) {
+        return steps+X-Y;
+    }
+    
+    if (X < Y) {
+        Y%2 != 0 && Y++ && steps++
+        Y  = Y/2;
+        steps++;
+        steps = brokenCalc(X,Y,steps);
+    }
+    
+    return steps;
+};
