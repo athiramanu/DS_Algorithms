ff88c1ad66d5587260d3c9d7811910cc203ae45f
athiramanu <athiramanu9400@gmail.com>
2021-05-15 19:20:13 +0530

    Maximum Depth of N-ary Tree

---
+++ tree/maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,24 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number}
+ */
+ var maxDepth = function(root) {
+    
+    if (!root) return 0
+
+    let depth = 0;
+    
+    for (let child of root.children) {
+        depth = Math.max(depth, maxDepth(child));
+    }
+        
+    return depth+1;
+};
