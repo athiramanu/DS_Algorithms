8c858f7a7ec8df3007da47243150bb1233427392
athiramanu <athiramanu9400@gmail.com>
2021-05-16 11:32:32 +0530

    N-ary Tree Preorder Traversal

---
+++ tree/n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.js
@@ -0,0 +1,22 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+ var preorder = function(root, result = []) {
+    
+    if (!root) return result;
+    
+    result.push(root.val);
+    
+    for (let child of root.children) preorder(child, result);
+    
+    return result;
+};
